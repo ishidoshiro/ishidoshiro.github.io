f22c87693014ac53263f9dbf54425e3132d5abf4
ishidoshiro <81201896+ishidoshiro@users.noreply.github.com>
2026-02-14 05:46:01 +0000

    Deploying to gh-pages from @ ishidoshiro/ishidoshiro.github.io@de16fb0b60bcf0fe2a4ea8d2fc05efbe952ed869 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,14 +3,14 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-koji-ishidoshiro",
-    title: "Koji Ishidoshiro",
+    id: "nav-top",
+    title: "Top",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-koji-ishidoshiro",
-          title: "Koji Ishidoshiro",
+  },{id: "nav-top",
+          title: "Top",
           description: "",
           section: "Navigation",
           handler: () => {
